3770799fec88b13f165db6bf0ba21c8b2ee17cf9
Arturo Escudero <darkturo@gmail.com>
2017-05-22 03:19:45 +0200

    Integrate an existen javascript sketch script
    Using an opensource javscript sketch script based on jquery.
    
    The script uses the html5 canvas, and allows for different colors and
    strokes.

--- lib/whiteboard/__init__.py
+++ lib/whiteboard/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import os.path
 
-template_folder = os.path.normpath( os.path.join(__file__, "..", "..", "..", "templates") )
+project_base_dir = os.path.normpath( os.path.join(__file__, "..", "..", ".." ) )
+template_folder = os.path.join(project_base_dir, "templates")
+static_folder = os.path.join(project_base_dir, "static")
 
-app = Flask(__name__, template_folder=template_folder)
+app = Flask(__name__, template_folder=template_folder, 
+                      static_folder=static_folder)
